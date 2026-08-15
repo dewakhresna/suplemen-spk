6bf36bafd0f902ba755b54a5c6b07834a4804576
dewa khresna <dewakhresna04@gmail.com>
2026-08-16 02:52:31 +0700

    Add Config

--- frontend-spk-suplemen/src/config/environment.ts
+++ frontend-spk-suplemen/src/config/environment.ts
@@ -1,6 +1,6 @@
 const environment = {
     API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
-    Domain: "http://localhost:5000"
+    Domain: process.env.NEXT_PUBLIC_DOMAIN || "http://localhost:5000"
 }
 
 export default environment;
--- frontend-spk-suplemen/src/utils/api.ts
+++ frontend-spk-suplemen/src/utils/api.ts
@@ -1,9 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
+import environment from "@/config/environment";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', 
+  baseURL: environment.API_URL,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
